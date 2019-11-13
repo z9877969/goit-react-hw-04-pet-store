41425af3fd1dc9da6d6f36084d809191cbd1f737
Andriy Kyrylenko <z9877969@ukr.net>
2019-11-13 12:32:17 +0200

    Updates

---
+++ precache-manifest.7336761a9372bf50e83bb01b3e04326f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf607f2924cfa1f10f94eb7cda03dfa",
+    "url": "/goit-react-hw-04-pet-store/index.html"
+  },
+  {
+    "revision": "23653b6bed97149efe4d",
+    "url": "/goit-react-hw-04-pet-store/static/css/main.aa76a120.chunk.css"
+  },
+  {
+    "revision": "364714802888b71c72b6",
+    "url": "/goit-react-hw-04-pet-store/static/js/2.d1f06ee4.chunk.js"
+  },
+  {
+    "revision": "23653b6bed97149efe4d",
+    "url": "/goit-react-hw-04-pet-store/static/js/main.d8a66e68.chunk.js"
+  },
+  {
+    "revision": "85d28d4578ed648bcf2a",
+    "url": "/goit-react-hw-04-pet-store/static/js/runtime-main.03d6a3d1.js"
+  }
+]);
